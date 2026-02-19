95dc601df737cdf89f319eb484d3c51562cce09b
elebur <elebur249@proton.me>
2026-02-19 16:40:55 +0200

    feat: now projects can be deleted

--- tripplan/api/urls.py
+++ tripplan/api/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from api.views.project import create_project, get_project
+from api.views.project import create_project, process_project
 
 urlpatterns = [
     path("project/", create_project, name="create_project"),
-    path("project/<int:pk>/", get_project, name="get_project"),
+    path("project/<int:pk>/", process_project, name="process_project"),
 ]
--- tripplan/api/views/project.py
+++ tripplan/api/views/project.py
@@ -1,8 +1,11 @@
+from django.http import HttpResponseNotAllowed
 from django.shortcuts import get_object_or_404
+from django.views.decorators.csrf import csrf_exempt
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.request import Request
 from rest_framework.response import Response
+from rest_framework.serializers import ValidationError
 
 from api.seralizers import ProjectSerializer
 from core.models import Project
@@ -23,8 +26,32 @@ def create_project(request: Request) -> Response:
     return Response(result, status=status_code)
 
 
+@csrf_exempt
+def process_project(request: Request, pk) -> Response | HttpResponseNotAllowed:
+    if request.method == "GET":
+        return get_project(request, pk)
+    if request.method == "DELETE":
+        return delete_project(request, pk)
+
+    msg = f"""{{"detail": "Method \\"{request.method}\\" not allowed."}}"""
+    return HttpResponseNotAllowed(content=msg, permitted_methods=("GET", "POST"))
+
+
 @api_view(["GET"])
 def get_project(request: Request, pk: int) -> Response:
     p = get_object_or_404(Project, pk=pk)
     serial = ProjectSerializer(p, many=False)
     return Response(serial.data)
+
+
+@api_view(["DELETE"])
+def delete_project(request: Request, pk: int) -> Response:
+    project = get_object_or_404(Project, pk=pk)
+    project_id = project.id
+    if project.places.filter(visited = True).exists():
+        msg = (f"The project<{project_id}> cannot be deleted because it has "
+               "places that were visited")
+        raise ValidationError({"details": msg}, code=status.HTTP_409_CONFLICT)
+
+    project.delete()
+    return Response({"details": f"The project<{project_id}> has been deleted."})
