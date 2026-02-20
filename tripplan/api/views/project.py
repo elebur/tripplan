855753f64617f08ef044578e30cbd5cec0f002c3
elebur <elebur249@proton.me>
2026-02-20 12:00:01 +0200

    fix: add return code

--- tripplan/api/views/project.py
+++ tripplan/api/views/project.py
@@ -89,6 +89,6 @@ def update_project(request: Request, pk: int) -> Response:
 
     if serializer.is_valid():
         serializer.save()
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
